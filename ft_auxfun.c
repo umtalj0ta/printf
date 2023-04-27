e7ea76d27157513d95e159a1bc56aeb8282f3c39
Joao Carlos Gomes Vieira <jgomes-v@c1r6s6.42porto.com>
2023-04-27 16:44:48 +0100

    half workinh

---
+++ ft_auxfun.c
@@ -0,0 +1,70 @@
+#include "ft_printf.h"
+
+char	ft_printchar(int c)
+{
+  int i;
+
+  i = 0;
+	write(1, &c, 1);
+  i += 1;
+	return (i);
+}
+
+void	ft_putstr(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		ft_printchar(s[i]);
+		i++;
+	}
+}
+
+int 	ft_putpercent(void)
+{
+	return (ft_printchar('%'));
+  return(1);
+}
+
+int	ft_printstr(char *s)
+{
+	int	i;
+  if (!s)
+	{
+		ft_putstr("(null)");
+		return (6);
+	}
+	i = 0;
+	while (s[i])
+	{
+		ft_printchar(s[i]);
+		i++;
+	}
+	return (i);
+}
+
+int ft_printnbr(int n)
+{
+  int len;
+  char *nbr;
+  long long ln;
+
+  len = 0;
+  ln = n;
+  if(ln < 0 && ln != -2147483648)
+  {
+    len += ft_printchar('-');
+    ln *= -1;
+  }
+  if( ln == -2147483648)
+  {
+    ft_printstr("-2147483648");
+    return(11);
+  }  
+  nbr = ft_itoa(ln);
+  len += ft_printstr(nbr);
+  free(nbr);
+return(len);
+}
---
+++ ft_printf.c
@@ -0,0 +1,53 @@
+#include "ft_printf.h"
+
+int	ft_formats(va_list args, char c)
+{
+	int	buffer;
+
+	buffer = 0;
+	if (c == 'c')
+		buffer += ft_printchar(va_arg(args, int));
+	else if (c == '%')
+		buffer += ft_putpercent();
+	else if (c == 's')
+		buffer += ft_printstr(va_arg(args, char *));
+  else if (c == 'i' || c == 'd')
+    buffer += ft_printnbr(va_arg(args,int));
+  
+  return(buffer);
+}
+
+int	ft_printf(const char *str, ...)
+{
+	va_list			args;
+	int				buffer;
+	unsigned int	i;
+
+	i = 0;
+	buffer = 0;
+	va_start(args, str);
+	while (str[i])
+	{
+		if (str[i] == '%')
+		{
+			i++;
+			buffer += ft_formats(args, str[i]);
+		}
+		else
+		{
+			ft_printchar(str[i]);
+			buffer++;
+		}
+		i++;
+	}
+	va_end(args);
+	return (buffer);
+}
+
+// int	main(void)
+// {
+// 	int teste = -2147483648;
+// int teste2 = -21;
+// 	ft_printf("%i\n", teste);
+//   	ft_printf("%i", teste2);
+// }
